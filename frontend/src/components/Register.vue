247faf7f236f360c2f593901f79af4feadd64f8e
Anurag <akanuragkumar4@gmail.com>
2024-11-20 11:38:00 +0530

    code changed

--- frontend/src/components/Register.vue
+++ frontend/src/components/Register.vue
@@ -65,8 +65,8 @@ export default {
             }
 
             try {
-                // const response = await fetch('https://trial-ria-app.onrender.com/register', {
-                const response = await fetch('http://127.0.0.1:5000/register', {
+                const response = await fetch('https://trial-ria-app.onrender.com/register', {
+                // const response = await fetch('http://127.0.0.1:5000/register', {
                     method: 'POST',
                     headers: {
                         'Content-Type': 'application/json',
